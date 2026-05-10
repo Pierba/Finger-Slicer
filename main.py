d48385a23f3b9e3f354cfc225b2f951c7ddda652
MercuriLorenzo <mercuri.2145783@studenti.uniroma1.it>
2026-05-10 18:18:50 +0200

    .

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ from pathlib import Path
 import config as cfg
 from extract_assets import extract_assets
 
-def args_parser():
+def args_parser() -> argparse.Namespace:
     # Init parser
     parser = argparse.ArgumentParser(description="")
 
     # Flags
     parser.add_argument(
-        "--image", 
-        type=Path, 
-        required=True, 
+        "--image",
+        type=Path,
+        required=True,
         help="Path to the input image."
     )
     parser.add_argument(
-        "--assets", 
-        type=Path, 
-        default=cfg.ASSETS, 
+        "--assets",
+        type=Path,
+        default=cfg.ASSETS,
         help="Path to the output image."
     )
     
